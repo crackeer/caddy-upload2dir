b4ef3eef0921743a3153d8a92bbd6a52995222f6
liuhu016 <liuhu016@realsee.com>
2023-02-23 14:51:45 +0800

    Delete

--- upload2dir.go
+++ upload2dir.go
@@ -1,12 +1,14 @@
 package upload2dir
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
@@ -17,7 +19,7 @@ import (
 )
 
 const (
-	Version = "0.9"
+	Version = "1.0"
 )
 
 func init() {
@@ -28,20 +30,13 @@ func init() {
 // Middleware implements an HTTP handler that writes the
 // uploaded file  to a file on the disk.
 type Upload2dir struct {
-	DestDirField     string `json:"dest_dir_field,omitempty"`
-	FileFieldName    string `json:"file_field_name,omitempty"`
-	MaxFilesize      int64  `json:"max_filesize_int,omitempty"`
-	MaxFilesizeH     string `json:"max_filesize,omitempty"`
-	MaxFormBuffer    int64  `json:"max_form_buffer_int,omitempty"`
-	MaxFormBufferH   string `json:"max_form_buffer,omitempty"`
-	ResponseTemplate string `json:"response_template,omitempty"`
-
-	MyTlsSetting struct {
-		InsecureSkipVerify bool   `json:"insecure,omitempty"`
-		CAPath             string `json:"capath,omitempty"`
-	}
-
-	// TODO: Handle notify Body
+	FileServerRoot string `json:"file_server_root"`
+	DestField      string `json:"dest_field,omitempty"`
+	FileFieldName  string `json:"file_field_name,omitempty"`
+	MaxFilesize    int64  `json:"max_filesize_int,omitempty"`
+	MaxFilesizeH   string `json:"max_filesize,omitempty"`
+	MaxFormBuffer  int64  `json:"max_form_buffer_int,omitempty"`
+	MaxFormBufferH string `json:"max_form_buffer,omitempty"`
 
 	ctx    caddy.Context
 	logger *zap.Logger
@@ -62,24 +57,11 @@ func (u *Upload2dir) Provision(ctx caddy.Context) error {
 
 	repl := caddy.NewReplacer()
 
-	if u.DestDirField == "" {
-		u.logger.Error("Provision",
-			zap.String("msg", "no Destination Directory specified (dest_dir)"))
-		return fmt.Errorf("no Destination Directory specified (dest_dir)")
-	}
-
 	if u.FileFieldName == "" {
 		u.logger.Warn("Provision",
 			zap.String("msg", "no FileFieldName specified (file_field_name), using the default one 'myFile'"),
 		)
-		u.FileFieldName = "myFile"
-	}
-
-	if u.ResponseTemplate == "" {
-		u.logger.Warn("Provision",
-			zap.String("msg", "no ResponseTemplate specified (response_template), using the default one"),
-		)
-		u.ResponseTemplate = "upload-resp-template.txt"
+		u.FileFieldName = "file"
 	}
 
 	if u.MaxFilesize == 0 && u.MaxFilesizeH != "" {
@@ -140,14 +122,11 @@ func (u *Upload2dir) Provision(ctx caddy.Context) error {
 
 	u.logger.Info("Current Config",
 		zap.String("Version", Version),
-		zap.String("dest_dir_field", u.DestDirField),
+		zap.String("dest_field", u.DestField),
 		zap.Int64("max_filesize_int", u.MaxFilesize),
 		zap.String("max_filesize", u.MaxFilesizeH),
 		zap.Int64("max_form_buffer_int", u.MaxFormBuffer),
 		zap.String("max_form_buffer", u.MaxFormBufferH),
-		zap.String("response_template", u.ResponseTemplate),
-		zap.String("capath", u.MyTlsSetting.CAPath),
-		zap.Bool("insecure", u.MyTlsSetting.InsecureSkipVerify),
 	)
 
 	return nil
@@ -162,6 +141,74 @@ func (u *Upload2dir) Validate() error {
 // ServeHTTP implements caddyhttp.MiddlewareHandler.
 func (u Upload2dir) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 
+	if r.Method == http.MethodPut {
+		return u.PutFile(w, r, next)
+	}
+
+	if r.Method == http.MethodDelete {
+		return u.DeleteFile(w, r, next)
+	}
+
+	if r.Method == http.MethodPost {
+		return u.CreateDir(w, r, next)
+	}
+
+	return next.ServeHTTP(w, r)
+}
+
+// CreateDir
+//
+//	@receiver u
+//	@param w
+//	@param r
+//	@param next
+//	@return error
+func (u *Upload2dir) CreateDir(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	dest := r.FormValue(u.DestField)
+	if len(dest) < 1 {
+		dest = filepath.Join(u.FileServerRoot, r.URL.Path)
+	}
+
+	if dir, err := os.Stat(dest); err == nil && dir.IsDir() {
+		w.Write(wrapFailureData(fmt.Sprintf("dir %s exists", dest), -1))
+	} else {
+		if err := os.MkdirAll(dest, os.ModePerm); err != nil {
+			w.Write(wrapFailureData(fmt.Sprintf("mkdir %s error: %s", dest, err.Error()), -1))
+		} else {
+			w.Write(wrapSuccessData(nil))
+		}
+	}
+
+	return next.ServeHTTP(w, r)
+
+}
+
+func (u *Upload2dir) DeleteFile(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	dest := r.FormValue(u.DestField)
+	if len(dest) < 1 {
+		dest = filepath.Join(u.FileServerRoot, r.URL.Path)
+	}
+
+	err := os.Remove(dest)
+	w.WriteHeader(http.StatusOK)
+	if err != nil {
+		w.Write(wrapFailureData(fmt.Sprintf("delete %s error: %s", dest, err.Error()), -1))
+	} else {
+		w.Write(wrapSuccessData(nil))
+	}
+	return next.ServeHTTP(w, r)
+
+}
+
+// PutFile ServeHTTP
+//
+//	@receiver u
+//	@param w
+//	@param r
+//	@param next
+//	@return error
+func (u *Upload2dir) PutFile(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+
 	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
 
 	requuid, requuiderr := repl.GetString("http.request.uuid")
@@ -174,30 +221,15 @@ func (u Upload2dir) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 
 	repl.Set("http.upload.max_filesize", u.MaxFilesize)
 
-	r.Body = http.MaxBytesReader(w, r.Body, u.MaxFilesize)
-	if max_size_err := r.ParseMultipartForm(u.MaxFormBuffer); max_size_err != nil {
-		u.logger.Error("ServeHTTP",
-			zap.String("requuid", requuid),
-			zap.String("message", "The uploaded file is too big. Please choose an file that's less than MaxFilesize."),
-			zap.String("MaxFilesize", humanize.Bytes(uint64(u.MaxFilesize))),
-			zap.Error(max_size_err),
-			zap.Object("request", caddyhttp.LoggableHTTPRequest{Request: r}))
-		return caddyhttp.Error(http.StatusRequestEntityTooLarge, max_size_err)
-	}
-
 	// FormFile returns the first file for the given file field key
 	// it also returns the FileHeader so we can get the Filename,
 	// the Header and the size of the file
 	file, handler, ff_err := r.FormFile(u.FileFieldName)
-	destDir := r.FormValue(u.DestDirField)
-	if len(destDir) < 1 {
-		u.logger.Error("FormValue Error",
-			zap.String("requuid", requuid),
-			zap.String("message", "Error Retrieving the File"),
-			zap.Error(ff_err),
-			zap.Object("request", caddyhttp.LoggableHTTPRequest{Request: r}))
-		return caddyhttp.Error(http.StatusInternalServerError, fmt.Errorf("form field %s required", destDir))
+	dest := r.FormValue(u.DestField)
+	if len(dest) < 1 {
+		dest = filepath.Join(u.FileServerRoot, r.URL.Path)
 	}
+
 	if ff_err != nil {
 		u.logger.Error("FormFile Error",
 			zap.String("requuid", requuid),
@@ -208,12 +240,23 @@ func (u Upload2dir) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 	}
 	defer file.Close()
 
-	// Create the file within the DestDir directory
+	destDir, fileName := filepath.Split(dest)
 	if err := os.MkdirAll(destDir, os.ModePerm); err != nil {
 		return caddyhttp.Error(http.StatusInternalServerError, fmt.Errorf("mkdirall %s error: %s", destDir, err.Error()))
 	}
 
-	tempFile, tmpf_err := os.OpenFile(filepath.Join(destDir, handler.Filename), os.O_RDWR|os.O_CREATE, 0755)
+	if target, err := os.Stat(dest); err == nil && target.Size() > 0 {
+		if err := os.Rename(dest, filepath.Join(destDir, fmt.Sprintf("backup-%d.%s", time.Now().Unix(), fileName))); err != nil {
+			u.logger.Error("FormFile Error",
+				zap.String("requuid", requuid),
+				zap.String("message", "Rename file error"),
+				zap.Error(err),
+				zap.Object("request", caddyhttp.LoggableHTTPRequest{Request: r}))
+			return caddyhttp.Error(http.StatusInternalServerError, err)
+		}
+	}
+
+	tempFile, tmpf_err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE, 0755)
 
 	if tmpf_err != nil {
 		u.logger.Error("TempFile Error",
@@ -237,8 +280,6 @@ func (u Upload2dir) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 			zap.Object("request", caddyhttp.LoggableHTTPRequest{Request: r}))
 		return caddyhttp.Error(http.StatusInternalServerError, io_err)
 	}
-	// write this byte array to our temporary file
-	//tempFile.Write(fileBytes)
 
 	u.logger.Info("Successful Upload Info",
 		zap.String("requuid", requuid),
@@ -251,11 +292,27 @@ func (u Upload2dir) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 	repl.Set("http.upload.filename", handler.Filename)
 	repl.Set("http.upload.filesize", handler.Size)
 
-	if u.ResponseTemplate != "" {
-		r.URL.Path = "/" + u.ResponseTemplate
-	}
-
+	w.WriteHeader(http.StatusOK)
+	w.Write(wrapSuccessData(map[string]interface{}{
+		"dir": dest,
+	}))
 	return next.ServeHTTP(w, r)
+}
+
+func wrapSuccessData(data interface{}) []byte {
+	bytes, _ := json.Marshal(map[string]interface{}{
+		"code": 0,
+		"data": data,
+	})
+	return bytes
+}
+
+func wrapFailureData(message string, code int64) []byte {
+	bytes, _ := json.Marshal(map[string]interface{}{
+		"code":    code,
+		"message": message,
+	})
+	return bytes
 }
 
 // UnmarshalCaddyfile implements caddyfile.Unmarshaler.
@@ -265,8 +322,8 @@ func (u *Upload2dir) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 		for d.NextBlock(0) {
 			switch d.Val() {
 
-			case "dest_dir_field":
-				if !d.Args(&u.DestDirField) {
+			case "dest_field":
+				if !d.Args(&u.DestField) {
 					return d.ArgErr()
 				}
 			case "file_field_name":
@@ -313,19 +370,6 @@ func (u *Upload2dir) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 					return d.Errf("parsing max_filesize_int: %v", err)
 				}
 				u.MaxFilesize = i
-			case "response_template":
-				if !d.Args(&u.ResponseTemplate) {
-					return d.ArgErr()
-				}
-			case "insecure":
-				if !d.NextArg() {
-					return d.ArgErr()
-				}
-				u.MyTlsSetting.InsecureSkipVerify = true
-			case "capath":
-				if !d.Args(&u.MyTlsSetting.CAPath) {
-					return d.ArgErr()
-				}
 			default:
 				return d.Errf("unrecognized servers option '%s'", d.Val())
 			}
